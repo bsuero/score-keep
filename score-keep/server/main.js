e6ceaa860a2ff89e7fae2ba6b1bd514021da216c
brany suero <brany.suero@coface.com>
2018-09-11 14:47:33 -0400

    practicion importing and exporting

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -1,5 +1,7 @@
 import { greetUser } from "../imports/utils";
+import { add } from "../imports/math";
 
 console.log(greetUser());
 
 console.log('log from /server/main.js');
+console.log(add(3, 4));
